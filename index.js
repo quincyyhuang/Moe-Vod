b279b8bbf48104567f3bc00713232597db26873e
Quincy Huang <quincyyhuang@gmail.com>
2017-07-16 19:14:29 +0800

    login

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Dependencies
 var express = require('express')
 var router = require("./router")
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var FileStore = require('session-file-store')(session);
 
 //Global Variables
 var app = express()
@@ -14,6 +17,18 @@ app.use('/av', express.static('/av/av'))
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'))
 });
+
+//Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use the session middleware
+app.use(session({
+    store: new FileStore,
+	secret: 'sukebei',
+	resave: true,
+    saveUninitialized: true
+}));
 
 //Router
 router(app)
--- router.js
+++ router.js
@@ -1,46 +1,87 @@
 //Dependencies
 const url = require('url')
 const path = require('path')
-const querystring = require('querystring')
-var request = require('request')
 var fs = require('fs')
 
 //Data
 const videoExt = ['.mp4', '.rmvb']
+const filterString = ['台', '裸聊', '美女']
+const userCollection = {
+    'quincy': 'quincyhuang'
+}
 var videoList = []
 
 module.exports = function(app) {
     app.get('/', (req, res) => {
-        res.render('index')
+        res.render('index', {
+            'alert': false
+        })
+    })
+
+    app.post('/login', (req, res) => {
+        var u = req.body.username
+        var p = req.body.password
+        if (p == userCollection.u) {
+            // OK
+            req.session.login = true
+            res.redirect('/list')
+        } else {
+            req.session.login = false
+            res.render('index', {
+                'alert': 'invalidCertification'
+            })
+        }
     })
 
     app.get('/list', (req, res) => {
-        videoList = []
-        getAllVideo('/av/av')
-        var nameList = []
-        videoList.forEach((i) => {
-            nameList.push(path.basename(i, path.extname(i)))
-        })
-        res.render('list', {
-            'name': nameList,
-            'video': videoList
-        })
+        if (req.session.login) {
+            videoList = []
+            getAllVideo('/av/av')
+            var nameList = []
+            videoList.forEach((i) => {
+                nameList.push(path.basename(i, path.extname(i)))
+            })
+            res.render('list', {
+                'name': nameList,
+                'video': videoList
+            })
+        } else {
+            res.render('index', {
+                'alert': 'invalidSession'
+            })
+        }
     })
 
     app.get('/vod', (req, res) => {
-        var sourceMP4 = req.query.d.substring(3)
-        var title = path.basename(sourceMP4, path.extname(sourceMP4))
-        res.render('vod', {
-            'title': title,
-            'sourceMP4': sourceMP4
-        })
+        if (req.session.login) {
+            var sourceMP4 = req.query.d.substring(3)
+            var title = path.basename(sourceMP4, path.extname(sourceMP4))
+            res.render('vod', {
+                'title': title,
+                'sourceMP4': sourceMP4
+            })
+        } else {
+            res.render('index', {
+                'alert': 'invalidSession'
+            })
+        }
     })
 }
 
 function getAllVideo(p) {
     if (fs.statSync(p).isFile()) {
         //Check extension
-        if (videoExt.includes(path.extname(p))) videoList.push(p)
+        if (videoExt.includes(path.extname(p))) {
+            // Filter
+            var valid = true
+            for (var i = 0; i < filterString.length; i++) {
+                if (p.includes(filterString[i])) {
+                    valid = false
+                    break
+                }
+            }
+            if (valid == true) videoList.push(p)
+        }
     } else if (fs.statSync(p).isDirectory() && path.basename(p) != 'node_modules' && path.basename(p)[0] != '.') {
         var files = fs.readdirSync(p)
         files.forEach((f) => {
