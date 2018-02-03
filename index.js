48889b48c6b9c05164ce25eba74f0c626fd1a3db
Quincy Huang <quincyyhuang@gmail.com>
2018-02-03 18:21:02 +0800

    version 2.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var router = require("./router")
 var bodyParser = require('body-parser')
 var session = require('express-session')
-var FileStore = require('session-file-store')(session);
+var FileStore = require('session-file-store')(session)
 
 //Global Variables
 var app = express()
@@ -11,24 +11,22 @@ var app = express()
 //Settings
 app.set('port', (process.env.PORT || 1234))
 app.set('view engine', 'ejs')
-app.set('videoFolder', __dirname)
 app.use('/static', express.static(__dirname + '/static'))
-app.use('/av', express.static('/av/av'))
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'))
-});
+})
 
 //Body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Use the session middleware
 app.use(session({
     store: new FileStore,
-	secret: 'sukebei',
+	secret: 'quincyhuang',
 	resave: true,
     saveUninitialized: false
-}));
+}))
 
 //Router
-router(app)
+app.use('/', router)
--- router.js
+++ router.js
@@ -2,103 +2,116 @@
 const url = require('url')
 const path = require('path')
 var fs = require('fs')
+var express = require('express')
+const NodeCache = require("node-cache")
 
-//Data
-const videoExt = ['.mp4', '.rmvb']
-const filterString = ['台', '裸聊', '美女', 'UU23', '.net']
+// Data
 const userCollection = {
-    'quincy': 'quincyhuang',
-    'wjw': 'wangjiewen'
+    'quincy': 'quincyhuang123!'
 }
-var videoList = []
+const fileRoot = '/root/files'
+const videoExt = ['.mp4']
+var myCache = new NodeCache()
 
-module.exports = function(app) {
-    app.get('/', (req, res) => {
+// Router
+var router = express.Router()
+
+// Routes
+router.get('/', (req, res) => {
+    res.render('index', {
+        'alert': false
+    })
+})
+
+router.post('/login', (req, res) => {
+    var u = req.body.username
+    var p = req.body.password
+    if (p == userCollection[u]) {
+        // OK
+        req.session.login = true
+        res.redirect('/list')
+    } else {
+        req.session.login = false
         res.render('index', {
-            'alert': false
-        })
-    })
-
-    app.post('/login', (req, res) => {
-        var u = req.body.username
-        var p = req.body.password
-        if (p == userCollection[u]) {
-            // OK
-            req.session.login = true
-            res.redirect('/list')
-        } else {
-            req.session.login = false
-            res.render('index', {
-                'alert': 'invalidCertification'
-            })
-        }
-    })
-
-    app.get('/list', (req, res) => {
-        if (req.session.login) {
-            videoList = []
-            getAllVideo('/av/av')
-            var nameList = []
-            videoList.forEach((i) => {
-                nameList.push(path.basename(i, path.extname(i)))
-            })
-            res.render('list', {
-                'name': nameList,
-                'video': videoList
-            })
-        } else {
-            res.render('index', {
-                'alert': 'invalidSession'
-            })
-        }
-    })
-
-    app.get('/vod', (req, res) => {
-        if (req.session.login) {
-            var sourceMP4 = req.query.d.substring(3)
-            var title = path.basename(sourceMP4, path.extname(sourceMP4))
-            res.render('vod', {
-                'title': title,
-                'sourceMP4': sourceMP4
-            })
-        } else {
-            res.render('index', {
-                'alert': 'invalidSession'
-            })
-        }
-    })
-
-    app.get('/logout', (req, res) => {
-        // req.session.destroy((err) => {
-        //     if (err) console.log(err)
-        //     res.redirect('/')
-        // })
-        req.session.regenerate((err) => {
-            // will have a new session here
-            req.session.login = false
-            res.redirect('/')
-        })
-    })
-}
-
-function getAllVideo(p) {
-    if (fs.statSync(p).isFile()) {
-        //Check extension
-        if (videoExt.includes(path.extname(p))) {
-            // Filter
-            var valid = true
-            for (var i = 0; i < filterString.length; i++) {
-                if (p.includes(filterString[i])) {
-                    valid = false
-                    break
-                }
-            }
-            if (valid == true) videoList.push(p)
-        }
-    } else if (fs.statSync(p).isDirectory() && path.basename(p) != 'node_modules' && path.basename(p)[0] != '.') {
-        var files = fs.readdirSync(p)
-        files.forEach((f) => {
-            getAllVideo(path.join(p, f))
+            'alert': 'invalidCertification'
         })
     }
+})
+
+router.get('/logout', (req, res) => {
+    // req.session.destroy((err) => {
+    //     if (err) console.log(err)
+    //     res.redirect('/')
+    // })
+    req.session.regenerate((err) => {
+        // will have a new session here
+        req.session.login = false
+        res.redirect('/')
+    })
+})
+
+router.get('/list', (req, res) => {
+    var cwd = req.query.p || fileRoot
+    var token = generateToken()
+    myCache.set('token', token, 3600)
+    var info = getCWDInfo(cwd)
+    res.render('list', {
+        path: path,
+        token: token,
+        cwd: cwd,
+        info: info
+    })
+})
+
+router.get('/file', (req, res) => {
+    var file = req.query.p
+    if (!file) res.redirect('/list')
+    else {
+        if (videoExt.includes(path.extname(file))) {
+            if (req.session.login) {
+                var sourceMP4 = file
+                var title = path.basename(sourceMP4, path.extname(sourceMP4))
+                res.render('vod', {
+                    'title': title,
+                    'sourceMP4': sourceMP4
+                })
+            } else {
+                res.render('index', {
+                    'alert': 'invalidSession'
+                })
+            }
+        } else {
+            if (req.session.login || req.query.token == myCache.get('token')) res.download(file)
+            else res.status(403).end()
+        }
+    }
+})
+
+function getCWDInfo(cwd) {
+    var info = {
+        directories: [],
+        files: []
+    }
+    var items = fs.readdirSync(cwd)
+    items.forEach((i) => {
+        var abs = path.join(cwd, i)
+        if (fs.statSync(abs).isDirectory()) info.directories.push(abs)
+        else if (fs.statSync(abs).isFile()) info.files.push(abs)
+    })
+    return info
 }
+
+function generateToken() {
+    var choice = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
+    function getAnIndex() {
+        return Math.floor(Math.random() * 31)
+    }
+
+    var result = ''
+    for (var i = 0; i < 20; i++) result += choice[getAnIndex()]
+    return result
+}
+
+// Export
+module.exports = router
